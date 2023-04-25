4d0378a53d16a056ceee6b232d6199755bacfdc1
刘嘉惠 <jiahui.liu@geo.unibe.ch>
2023-04-25 17:15:37 +0800

    Update DiffusionModeling_Run.m

--- DiffusionModeling_Run.m
+++ DiffusionModeling_Run.m
@@ -1,3 +1,24 @@
+% This script reset the configuration file REE_diffusion_Liu2023.mat
+%
+% REE_diffusion_Liu2023 is a free code for rare earth element diffusion
+% modeling, which developed for Liu et al. 2023 manuscript "Ultra-fast mineral growth
+% during regional metamorphism"
+%
+% Copyright © 2022-2023 University of Bern, Institute of Geological
+% Sciences, Pierre Lanari, Jiahui Liu
+%
+% REE_diffusion_Liu2023 is free code: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or any 
+% later version.
+%
+% REE_diffusion_Liu2023 is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with REE_diffusion_Liu2023. If not, see https://www.gnu.org/licenses.
 
 %% Diffusion modeling for HREE+Y in melt (inherited by garnet)
 clear all
