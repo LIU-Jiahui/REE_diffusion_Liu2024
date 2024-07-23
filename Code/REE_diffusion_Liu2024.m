29b1c7567ad57ac37f49577afafccd9985607c0d
刘嘉惠 <jiahui.liu@geo.unibe.ch>
2024-07-23 09:55:29 +0800

    REE_diffusion_Liu2024

--- Code/REE_diffusion_Liu2024.m
+++ Code/REE_diffusion_Liu2024.m
@@ -1,67 +1,11 @@
-% REE_diffusion_Liu2023 is a free code for rare earth element diffusion
-% modeling, which developed for Liu et al. 2023 manuscript "Ultra-fast mineral growth
-% during regional metamorphism"
-%
-% Copyright © 2022-2023 University of Bern, Institute of Geological
-% Sciences, Pierre Lanari, Jiahui Liu
-%
-% REE_diffusion_Liu2023 is free code: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or any 
-% later version.
-%
-% REE_diffusion_Liu2023 is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-%
-% You should have received a copy of the GNU General Public License
-% along with REE_diffusion_Liu2023. If not, see https://www.gnu.org/licenses.
-
-%% Diffusion modeling for HREE+Y in melt (inherited by garnet)
-clear all
+%% Diffusion modeling for HREE+Y in melt (recorded in garnet)
+clear
 clc
 close all
 
-%% Set HREE elements and diffusion conditions
+%% Set HREE elements
 HREE_Name = {'Lu','Yb','Er','Ho','Dy','Y'};
 
-T_C = 800;                                                % Temperature in °C
-T_K = T_C + 273.15;                                       % Temperature in Kelvin
-T_K_sigma = 25;                                           % Uncertainties of temperature
-logD0 = [-3.74,-5.23,-3.39,-4.65,-4.60,-5.09];            % D0 at 800C (J/K), Table 3, Holycross & Watson, 2018
-logD0_sigma = [1.40,0.84,1.49,1.07,1.04,1.01];            % Uncertainties of D0
-Ea = [198.97,164.47,205.25,175.99,176.47,165.74];         % Ea at 800C (KJ), Table 3, Holycross & Watson, 2018
-Ea_sigma = [31.24,18.78,33.36,23.78,23.27,22.51];         % Uncertainties of Ea
-
-% Simulated experimental D using Monte Carlo
-N = 20;                                                 % No. of Tries 
-T_K_simu = T_K + T_K_sigma.*randn(N,1);                   % Simulated temperature
-logD0_simu = logD0 + logD0_sigma.*randn(N,1);             % Simulated logD0
-Ea_simu = Ea + Ea_sigma.*randn(N,1);                      % Simulated Ea
-D_experi_simu = (10.^(logD0_simu).*exp(-Ea_simu.*1000./8.314./T_K_simu)).*10.^12./(1./(3600*24)); % Simulated experimental D using Monte Carlo, uncertainties from Temerature, logD0 and Ea.
-
-% make sure each experimental D < 1.9e7, otherwise it exceeds the statistical range
-for i =1:size(D_experi_simu)
-    if max(D_experi_simu(i,:)) > 1.9e7
-        while 1
-            T_K_simu2 = T_K + T_K_sigma.*randn(1,1);
-            logD0_simu2 = logD0 + logD0_sigma.*randn(1,1);
-            Ea_simu2 = Ea + Ea_sigma.*randn(1,1);
-            D_simu2 = (10.^(logD0_simu2).*exp(-Ea_simu2.*1000./8.314./T_K_simu2)).*10.^12./(1./(3600*24));
-            if max(D_simu2) < 1.9e7
-                D_experi_simu(i,:) = D_simu2;
-                break
-            end
-        end
-    end
-end
-D_experi_simu_meangeo = exp(mean(log(D_experi_simu)));    % μ*, median (geometric mean) of simulated experimental D
-D_experi_simu_sigmageo = exp(std(log(D_experi_simu)));    % σ*, multiplicative standard deviation (geometric "deviation")
-D_experi_simu_upper = D_experi_simu_meangeo.*D_experi_simu_sigmageo;     % μ*×σ*
-D_experi_simu_lower = D_experi_simu_meangeo./D_experi_simu_sigmageo;     % μ*/σ*
-uppererror = D_experi_simu_upper - D_experi_simu_meangeo;
-lowererror = D_experi_simu_meangeo - D_experi_simu_lower;
 
 %% Extract data & Set initial profiles
 Profile_Name = {'Profile1','Profile2','Profile3','Profile4','Profile5','Profile6'};
@@ -125,6 +69,48 @@ Count_max_right{6} = 5;
 Count_mins{6} = 5;
 [Yi{6}] = SetInitialProf_Bilateral(Yref{6},Count_max_left{6},Count_max_right{6},Count_mins{6},HREE_Name);
 
+% % Plot curve
+% for i = 1:6
+%     figure
+%     plot(Yref{1}(:,i),'o')
+% end
+
+
+%% Set diffusion conditions
+T_C = 800;                                            % Temperature in °C
+T_K = T_C + 273.15;                                   % Temperature in Kelvin
+T_K_sigma = 25;                                       % Uncertainties of temperature
+logD0 = [-3.74,-5.23,-3.39,-4.65,-4.60,-5.09];        % D0 (J/K), Table 3, Holycross & Watson, 2018
+logD0_sigma = [1.40,0.84,1.49,1.07,1.04,1.01];        % Uncertainties of D0 (J/K)
+Ea = [198.97,164.47,205.25,175.99,176.47,165.74];     % Ea (KJ)
+Ea_sigma = [31.24,18.78,33.36,23.78,23.27,22.51];     % Uncertainties of Ea (KJ)
+% logD0 = [-3.82,-4.90,-3.98,-3.91,-4.17,-5.09];      % D0 (J/K), Table 4, Holycross & Watson, 2018
+% logD0_sigma = [1.57,1.28,1.35,1.55,1.28,1.01];      % Uncertainties of D0 (J/K)
+% Ea = [196.97,171.42,210.29,190.93,185.72,165.74];   % Ea (KJ)
+% Ea_sigma = [34.97,28.62,33.33,30.06,28.59,22.51];   % Uncertainties of Ea (KJ)
+
+% Simulated experimental D using Monte Carlo
+N = 3;                                             % No. of Tries 
+T_K_simu = T_K + T_K_sigma.*randn(N,1);               % Simulated temperatures
+nu = 1;
+rng('default') % for reproducibility
+U = copularnd('t',[1 0.95; 0.95 1],nu,N);             
+logD0_simu = logD0 + logD0_sigma.*U(:,1);             % Simulated logD0s
+Ea_simu = Ea + Ea_sigma.*U(:,2);                      % Simulated Eas
+D_experi_simu = (10.^(logD0_simu).*exp(-Ea_simu.*1000./8.314./T_K_simu)).*10.^12./(1./(3600*24)); % Simulated experimental D using Monte Carlo, uncertainties from Temerature, logD0 and Ea.
+
+figure
+D_experi_simu_meangeo = exp(mean(log(D_experi_simu)));               % median/geometric mean of D_simu (experimental)
+D_experi_simu_sigmageo = exp(std(log(D_experi_simu)));               % σ*(multiplicative standard deviation, geometric "deviation") = exp(σ)
+D_experi_simu_upper = D_experi_simu_meangeo.*D_experi_simu_sigmageo;             % μ*×σ*
+D_experi_simu_lower = D_experi_simu_meangeo./D_experi_simu_sigmageo;             % μ*/σ*
+xerrorbar = 1:1:length(HREE_Name);
+uppererror = D_experi_simu_upper - D_experi_simu_meangeo;
+lowererror = D_experi_simu_meangeo - D_experi_simu_lower;
+errorbar(xerrorbar,D_experi_simu_meangeo,lowererror,uppererror);
+ylim([0 20000])
+
+
 %% Estimate diffusion time, effective diffusion coeffiecient, and geometry parameter
 [D_eff,D_eff_meangeo,G] = deal(cell(1,length(Profile_Name))); % effective D, mean effective D and geometry parameter in the porosity medium
 Y = zeros(N,length(HREE_Name),length(Xi));
@@ -144,42 +130,31 @@ Timelower = Timemeangeo/Timesigmageo;             % μ*/σ*
 Timeconfiinterval = find(Time >= Timelower & Time <= Timeupper);      % confidence interval
 disp(['Method 1  ',num2str(Timemeangeo),'   + ',num2str(Timeupper-Timemeangeo), '   -',num2str(Timemeangeo-Timelower),'   n=',num2str(length(Timeconfiinterval))]);
 
+
 for i = 1:length(D_eff_meangeo)
-    D_eff_meangeo{i} = exp(mean(log(D_eff{i})));  % median (geometric mean) of effective D_simu (effective)
+     D_eff_meangeo{i} = exp(mean(log(D_eff{i}))); % median/geometric mean of D_simu (effective)? (make sure later)
 end
 
-% Plot experimental D and effective D (meangeo)
-xerrorbar = 1:1:5;
-errorbar(xerrorbar,D_experi_simu_meangeo(xerrorbar),lowererror(xerrorbar),uppererror(xerrorbar));
-hold on
-Dff_mean = {'error','Deff_mean_tr1','Deff_mean_tr2','Deff_mean_tr3','Deff_mean_tr4','Deff_mean_tr5','Deff_mean_tr6'}
-for i = 1:length(D_eff_meangeo)
-    plot(xerrorbar,D_eff_meangeo{i}(1:length(xerrorbar)),'o')
-    hold on
-end
-legend(Dff_mean)
-ax = gca;
-ax.YScale = 'log';
-axis([0,7,1e1,1e6])
 
 % Estimate geometry parameter
-porosity = 0.1;                                   % ε (porosity), assumption as melt content = 10% in phase equilibrium modeling
+porosity = 0.1;                                   % assumption because of melt content = ~10% in phase equilibrium modeling
 for i = 1:length(G)
-    G{i} = porosity./(D_eff{i}./D_experi_simu);   % G means geometry parameter, the effective diffusion coefficient in porous media. Deff/Dexp = ε/G
+    G{i} = porosity./(D_eff{i}./D_experi_simu);   % G means geometry parameter, the effective diffusion coefficient in porous media. De/D0 = ε/G
 end
 
 G_HREE_Name = {'Lu','Yb','Er','Ho','Dy'};
 X_plot = 1:1:length(G_HREE_Name);                 % plot G for each profile
 figure
 for i = 1:length(G)
-    plot(X_plot,G{i}(:,1:5),'.','MarkerSize',8)
+    plot(X_plot,max(G{i}(:,1:5)),'.','MarkerSize',8)
+    plot(X_plot,min(G{i}(:,1:5)),'.','MarkerSize',8)
     plot(X_plot,mean(G{i}(:,1:5)),'o-','LineWidth',1.5)
     hold on
 end
 
 title('Geometric parameter')
 xlim([0 6])
-ylim([0 2.5])
+ylim([0 1])
 xticklabels(G_HREE_Name)
 legend(Profile_Name)
 newcolors = {'#F00','#F80','#FF0','#0B0','#00F','#F90'};
@@ -188,13 +163,13 @@ set(gca,'FontSize',40);
 set(gcf,'PaperSize',[25 25])
 
 %% Plot diffusion profile using fixed time & D
-[Y_model_expD,Y_model_effD] = deal(cell(1,length(Profile_Name)));
+[Y_model_epD,Y_model_effD] = deal(cell(1,length(Profile_Name)));
 for i = 1:length(Xi)
-    [Y_model_expD{i},R2_epD{i}] = diffprof_expD(HREE_Name,Timemeangeo,D_experi_simu_meangeo,Xi{i},Yi{i},Yref{i}); 
+    [Y_model_epD{i},R2_epD{i}] = diffprof_experiD(HREE_Name,Timemeangeo,D_experi_simu_meangeo,Xi{i},Yi{i},Yref{i}); 
     [Y_model_effD{i},R2_eff{i}] = diffprof_effD(HREE_Name,Timemeangeo,D_eff_meangeo{i},Xi{i},Yi{i},Yref{i});
 end
 
-Xi_nor = Xi;   % Reset the initial position of X (distance) as start from 0 micrometer.
+Xi_nor = Xi;   % Reset the X (distance) as start from 0 micrometer.
 for i = 1:length(Xi)
     Xi_nor{i} = Xi{i}-Xi{i}(1);
 end
@@ -207,7 +182,7 @@ for i = 1:length(HREE_Name)
         count = count + 1;
         plot3(Xi_nor{j},count.*(ones(length(Xi_nor{j}),1)),Yref{j}(:,i),'k.','MarkerSize',10)
         hold on
-        plot3(Xi_nor{j},count.*(ones(length(Xi_nor{j}),1)),Y_model_expD{j}(:,i),'b-','Linewidth',0.5)
+        plot3(Xi_nor{j},count.*(ones(length(Xi_nor{j}),1)),Y_model_epD{j}(:,i),'b-','Linewidth',0.5)
         hold on
         plot3(Xi_nor{j},count.*(ones(length(Xi_nor{j}),1)),Yi{j}(:,i),'r-','Linewidth',0.5)
         hold on
@@ -215,7 +190,7 @@ for i = 1:length(HREE_Name)
     title('Diffusion modeling profile (using experimental D)', HREE_Name(i))
     xlabel('Distance (μm)')
     ylabel('Profile No.')
-    zlabel('Mass fraction (μg/g)')
+    zlabel('Concentration (μg/g)')
     yticks([0:1:6])
     set(gca,'FontSize',40);
 end
@@ -236,7 +211,8 @@ for i = 1:length(HREE_Name)
     title('Diffusion modeling profile (using effective D)', HREE_Name(i))
     xlabel('Distance (μm)')
     ylabel('Profile No.')
-    zlabel('Mass fration (μg/g)')
+    zlabel('Concentration (μg/g)')
     yticks([0:1:6])
     set(gca,'FontSize',40);
 end
+
